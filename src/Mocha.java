e4fe5005ada0987a56a78909347f4612b5dcfb69
jamesbertel <berte005@cougars.csusm.edu>
2020-04-05 18:37:17 -0700

    added 2 files i guess

---
+++ src/Mocha.java
@@ -0,0 +1,12 @@
+public class Mocha {
+
+    public void start()
+    {
+        //Indicate the machine is rinnug
+        //display purchases type of coffee to LED
+        //Grind coffee beans for 15 seconds
+        //Heat up water to 200 degrees Fahrenheit
+        //Hold the temperature for 5 seconds
+        //After 15 seconds, set the LED to indicate the machine is not running
+    }
+}
